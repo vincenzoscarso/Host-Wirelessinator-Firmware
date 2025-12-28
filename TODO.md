4523cf78d29a4dddb70c3b3f4eabd6434db51dc3
vincenzoscarso <110780936+vincenzoscarso@users.noreply.github.com>
2025-12-28 20:31:18 +0100

    Changed TOML configs to JSON configs

--- TODO.md
+++ TODO.md
@@ -3,4 +3,5 @@
   - [x] write a "split" function and add it to the "utils.cpp" file
   - [x] convert the "mac_address" string obtained from the toml configs to a byte array before storing it in the "Host" object
   - [x] implement and use the "sendMagicPacket" function of "wifihandler"
-- [ ] Feature: Relay support (GND: GND, VCC: 3V3, OUT: D15)
+- [x] Feature: Relay support (GND: GND, VCC: 3V3, OUT: D15)
+- [x] Switch the toml configs with json configs
--- lib/ComponentHandler/Hosts.cpp
+++ lib/ComponentHandler/Hosts.cpp
@@ -1,12 +1,11 @@
 #include <Hosts.h>
 #include <logHandler.h>
+#include <nlohmann/json.hpp>
 #include <secrets.h>
-#include <toml++/toml.hpp>
 #include <stdexcept>
+using json = nlohmann::json;
 
 std::vector<Host> Hosts::hosts;
-
-static toml::parse_result& __get_hosts_table();
 
 bool Hosts::updateHostsVector() {
 	if (Hosts::getNumberOfHosts() <= 0) {
@@ -19,12 +18,12 @@ bool Hosts::updateHostsVector() {
 
 		try {
 			hosts.emplace_back(
-			    __get_hosts_table()[key]["name"].value_or<std::string>(""),
-			    __get_hosts_table()[key]["type"].value_or<std::string>(""),
-			    __get_hosts_table()[key]["control_options"]["use_relay"].value_or<bool>(false),
-			    __get_hosts_table()[key]["control_options"]["relay_pin"].value_or<int>(-1),
-			    __get_hosts_table()[key]["control_options"]["use_magic_packet"].value_or<bool>(false),
-			    __get_hosts_table()[key]["control_options"]["mac_address"].value_or<std::string>(""));
+			    secrets::hosts_json[key].value("name", ""),
+			    secrets::hosts_json[key].value("type", ""),
+			    secrets::hosts_json[key]["control_options"].value("use_relay", false),
+			    secrets::hosts_json[key]["control_options"].value("relay_pin", -1),
+			    secrets::hosts_json[key]["control_options"].value("use_magic_packet", false),
+			    secrets::hosts_json[key]["control_options"].value("mac_address", ""));
 		} catch (const std::exception& e) {
 			printErrorMessage("Failed to obtain values from the table: %s", e.what());
 			return false;
@@ -38,7 +37,7 @@ bool Hosts::updateHostsVector() {
  * @returns number of hosts if existent, else 0
  */
 int Hosts::getNumberOfHosts() {
-	return __get_hosts_table()["number_of_hosts"].value_or<int>(0);
+	return secrets::hosts_json.value("number_of_hosts", 0);
 }
 
 bool Hosts::isHostNameValid(const std::string& host_name) {
@@ -57,9 +56,4 @@ int Hosts::getHostVectorIndexFromHostName(const std::string& host_name) {
 		}
 	}
 	throw std::invalid_argument("Couldn't find the host object: inexistent host name");
-}
-
-static toml::parse_result& __get_hosts_table() {
-	static toml::parse_result __hosts_table = toml::parse(secrets::hosts_toml);
-	return __hosts_table;
 }
