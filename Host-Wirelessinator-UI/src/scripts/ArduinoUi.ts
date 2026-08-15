ca5709ab3a8808f493b0fb709044851380dc4842
vincenzoscarso <110780936+vincenzoscarso@users.noreply.github.com>
2026-08-15 10:26:44 +0200

    Moved the WebUI into another repository

--- Host-Wirelessinator-UI/src/scripts/ArduinoUi.ts
+++
@@ -1,6 +0,0 @@
-import { get } from './utils'
-
-export const arduino_ui_node = {
-	status: get<HTMLSpanElement>("arduino-status"),
-	keep_connection_checkbox: get<HTMLInputElement>("arduino-keep-connection-checkbox")
-}
--- Host-Wirelessinator-UI/vite.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    base: './',
-    build: {
-        outDir: './dist',
-        emptyOutDir: true,
-    }
-}
