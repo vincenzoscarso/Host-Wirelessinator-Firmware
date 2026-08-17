64d22e8e224466748a1f56d6f9473c82f6aa2690
vincenzoscarso <110780936+vincenzoscarso@users.noreply.github.com>
2026-08-17 10:58:27 +0200

    Another minor fix to logging messages

--- lib/ServerHandler/commandHandler.cpp
+++ lib/ServerHandler/commandHandler.cpp
@@ -72,7 +72,7 @@ bool __checkForCommandAndExcecuteInHostCommandsMap(websockets::WebsocketsClient&
 
 	if (command != host_commands_map.end()) {
 		if (!Hosts::isHostNameValid(host_name)) {
-			printErrorMessage(true, "Invalid host name '%s'", host_name.c_str());
+			printErrorMessage( "Invalid host name '%s'", host_name.c_str());
 			return false;
 		}
 
@@ -97,7 +97,7 @@ bool __checkForCommandAndExcecuteInDeviceCommandsMap(websockets::WebsocketsClien
   ============*/
 
 void commandHandler::handleNoCommand(websockets::WebsocketsClient& client, const std::string& message) {
-	printErrorMessage(true, "Unknown command: %s", message.c_str());
+	printErrorMessage( "Unknown command: %s", message.c_str());
 }
 
 /*----------------------------
@@ -116,37 +116,37 @@ void __handleBootCommand(websockets::WebsocketsClient& client, Host host) {
 		delay(1000);
 		componentHandler::setHostRelayPinStatus(host, HIGH);
 	} else {
-		printErrorMessage(true, "Couldn't find a method to boot this host, check configuration");
+		printErrorMessage( "Couldn't find a method to boot this host, check configuration");
 		return;
 	}
 
 	body += host.getName().c_str();
 	std::string response = __getResponse("Boot", body);
 	client.send(response.c_str());
-	printInfoMessage(true, "Sent response:\n%s", response.c_str());
+	printInfoMessage( "Sent response:\n%s", response.c_str());
 }
 
 void __handleRebootCommand(websockets::WebsocketsClient& client, Host host) {
 	std::string body = "Reboot command is currently not implemented for host: " + host.getName();
 	std::string response = __getResponse("Reboot", body);
 	client.send(response.c_str());
-	printInfoMessage(true, "Sent response:\n%s", response.c_str());
+	printInfoMessage( "Sent response:\n%s", response.c_str());
 }
 
 void __handleForceShutdownCommand(websockets::WebsocketsClient& client, Host host) {
 	std::string body = "ForceShutdown command is currently not implemented for host: " + host.getName();
 	std::string response = __getResponse("ForceShutdown", body);
 	client.send(response.c_str());
-	printInfoMessage(true, "Sent response:\n%s", response.c_str());
+	printInfoMessage( "Sent response:\n%s", response.c_str());
 
-	/* printInfoMessage(true, "Force shutting down system on host: %s", host.getName().c_str());
+	/* printInfoMessage( "Force shutting down system on host: %s", host.getName().c_str());
 
 	if (host.isUseRelayPinEnabled()) {
 	    componentHandler::setHostRelayPinStatus(host, HIGH);
 	    delay(5000);
 	    componentHandler::setHostRelayPinStatus(host, LOW);
 	} else {
-	    printErrorMessage(true, "Couldn't force-shutdown this host: relay needed, check configuration");
+	    printErrorMessage( "Couldn't force-shutdown this host: relay needed, check configuration");
 	} */
 }
 
@@ -157,7 +157,7 @@ void __handleGetStatusCommand(websockets::WebsocketsClient& client, Host host) {
 
 	std::string response = __getResponse("GetStatus", body.dump());
 	client.send(response.c_str());
-	printInfoMessage(true, "Sent response:\n%s", response.c_str());
+	printInfoMessage( "Sent response:\n%s", response.c_str());
 }
 
 /*------------------------------
@@ -167,7 +167,7 @@ void __handleGetStatusCommand(websockets::WebsocketsClient& client, Host host) {
 void __handleGetHostsJsonCommand(websockets::WebsocketsClient& client) {
 	std::string response = __getResponse("GetHostsJson", secrets::hosts_json.dump());
 	client.send(response.c_str());
-	printInfoMessage(true, "Sent response:\n%s", response.c_str());
+	printInfoMessage( "Sent response:\n%s", response.c_str());
 }
 
 void __handleInformationsCommand(websockets::WebsocketsClient& client) {
@@ -179,7 +179,7 @@ void __handleInformationsCommand(websockets::WebsocketsClient& client) {
 
 	auto response = __getResponse("Informations", body.dump());
 	client.send(response.c_str());
-	printInfoMessage(true, "Sent response:\n%s", response.c_str());
+	printInfoMessage( "Sent response:\n%s", response.c_str());
 }
 
 void __handleHelpCommand(websockets::WebsocketsClient& client) {
@@ -196,7 +196,7 @@ void __handleHelpCommand(websockets::WebsocketsClient& client) {
 
 	auto response = __getResponse("Help", body.dump());
 	client.send(response.c_str());
-	printInfoMessage(true, "Sent response:\n%s", response.c_str());
+	printInfoMessage( "Sent response:\n%s", response.c_str());
 }
 
 /*===========
